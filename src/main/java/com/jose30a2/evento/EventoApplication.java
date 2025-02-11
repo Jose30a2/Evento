6af16da71d846a39edec69130c8b151e203b1cb7
Jose Antonio <jose30a2@gmail.com>
2025-02-11 17:55:31 -0300

    Configuracao H2 Database

---
+++ src/main/java/com/jose30a2/evento/EventoApplication.java
@@ -0,0 +1,13 @@
+package com.jose30a2.evento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jose30a2/evento/EventoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jose30a2.evento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
